ea3e905b49cd573325cdf3b25e2f07197fb7306a
Anapaula <L23100201@gmail.com>
2026-02-09 17:13:31 -0600

    segundo commit

---
+++ MaquinaDeTuring/MaquinaDeTuring/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Console.WriteLine("Hello, World!");*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CajeroAutomatico
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Bienvenidx a la Maquina de Turing Pro");
+            Console.WriteLine("presione cualquier tecla para continuar");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("Te proporcionaremos el alfabeto... porfa no lo olvidesw :)");
+            Console.WriteLine("Alfabeto: {a,b,c,d}");
+            Console.WriteLine("presione cualquier tecla para continuar");
+            Console.ReadKey();  
+            Console.Clear();
+
+            Console.WriteLine("porfavor ingresa la cadena");
+            Console.ReadLine();
+            Console.WriteLine("presione cualquier tecla para continuar");
+            Console.ReadKey();
+            Console.Clear();
+
+            Console.WriteLine("La cadena es aceptada por la Maquina de Turing Pro");
+
+
+        }
+    }
+}
