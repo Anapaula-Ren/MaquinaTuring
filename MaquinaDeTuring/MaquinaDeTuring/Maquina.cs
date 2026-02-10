7d5f9fb62ceb57b6be984229916c6d3d968f9fb7
Anapaula <L23100201@gmail.com>
2026-02-09 18:55:39 -0600

    commit 3

---
+++ MaquinaDeTuring/MaquinaDeTuring/Maquina.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaquinaDeTuring
+{
+    public class Maquina
+    {
+        public List<char> Cinta { get; set; }
+        public int Cabezal { get; set; }
+        public char SimboloGuardado { get; set; }
+        public string EstadoActual { get; set; }
+
+        public Maquina(string cadenaInicial, int posicionInicial)
+        {
+            Cinta=cadenaInicial.ToList();
+            Cabezal=posicionInicial;
+            SimboloGuardado=' ';
+            EstadoActual = "Inicio";
+        }
+
+        public void MoverDerecha()
+        {
+           Console.WriteLine("Moviendo el cabezal a la derecha...");
+        }
+        public void MoverIzquierda()
+        {
+            Console.WriteLine("Moviendo el cabezal a la izquierda...");
+        }
+        public void Escribir(char simbolo)
+        {
+            Console.WriteLine($"Escribiendo el símbolo '{simbolo}' en la cinta...");
+        }
+    } 
+}
--- MaquinaDeTuring/MaquinaDeTuring/Program.cs
+++ MaquinaDeTuring/MaquinaDeTuring/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 /*Console.WriteLine("Hello, World!");*/
+using MaquinaDeTuring;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,18 +21,83 @@ namespace CajeroAutomatico
             Console.WriteLine("Te proporcionaremos el alfabeto... porfa no lo olvidesw :)");
             Console.WriteLine("Alfabeto: {a,b,c,d}");
             Console.WriteLine("presione cualquier tecla para continuar");
-            Console.ReadKey();  
-            Console.Clear();
-
-            Console.WriteLine("porfavor ingresa la cadena");
-            Console.ReadLine();
-            Console.WriteLine("presione cualquier tecla para continuar");
             Console.ReadKey();
             Console.Clear();
 
-            Console.WriteLine("La cadena es aceptada por la Maquina de Turing Pro");
+            Console.WriteLine("porfavor ingresa la cadena");
+            string cadena = Console.ReadLine();
+
+            Console.WriteLine("Ingrese la posicion inicial del cabezal");
+            int posicion = int.Parse(Console.ReadLine());
 
 
+
+            Maquina miMaquina = new Maquina(cadena, posicion);
+            EjecutarMenu(miMaquina);
         }
+
+            static void EjecutarMenu(Maquina m)
+            {
+                bool salir = false;
+                while (!salir)
+                {
+                    Console.Clear();
+                    DibujarInterfaz(m);
+
+                    Console.WriteLine("Seleccione una acción:");
+                    Console.WriteLine("1. Mover Derecha");
+                    Console.WriteLine("2. Mover Izquierda");
+                    Console.WriteLine("3. Escribir Símbolo");
+                    Console.WriteLine("4. Salir");
+
+                    Console.Write("\n Opcion: ");
+
+                    string opcion = Console.ReadLine();
+
+                    switch (opcion)
+                    {
+                        case "1":
+                            m.MoverDerecha();
+                            break;
+                        case "2":
+                            m.MoverIzquierda();
+                            break;
+                        case "3":
+                            Console.Write("Ingrese el símbolo a escribir {a,b,c,d}: ");
+                            char simbolo = Console.ReadKey().KeyChar;
+                            m.Escribir(simbolo);
+                            break;
+                        case "4":
+                            salir = true;
+                            break;
+                    }
+                }
+            }
+
+        static void DibujarInterfaz(Maquina m)
+        {
+            Console.WriteLine("\n========================================");
+            Console.WriteLine($" ESTADO ACTUAL: {m.EstadoActual}");
+            Console.WriteLine("========================================");
+
+            // Dibujar celdas de la cinta
+            Console.Write(" CINTA: ");
+            foreach (char c in m.Cinta)
+            {
+                Console.Write($"[{c}] ");
+            }
+            Console.WriteLine();
+
+            // Dibujar el cabezal (la flechita)
+            Console.Write("        ");
+            for (int i = 0; i < m.Cabezal; i++)
+            {
+                Console.Write("    "); // Espacios para alinear con las celdas
+            }
+            Console.WriteLine("  ^");
+            Console.WriteLine($"\n Memoria (Guardado): [{m.SimboloGuardado}]");
+            Console.WriteLine("========================================");
+        }
+
     }
 }
